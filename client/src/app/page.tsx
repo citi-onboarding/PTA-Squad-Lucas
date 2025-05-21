d469e5e1f947b737d83c59c5d9e9dfb43ed626ad
adsonviana <adsonaviana@gmail.com>
2025-05-21 10:45:45 -0300

    topBar version1 - visual elements

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,44 @@
 import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { LogoCITi, LeftTopBar, RightTopBar } from "../assets";
 
-import { LogoCITi } from "../assets";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col h-full justify-around items-center bg-black">
+    <div className="bg-red-100">
+      
+  
+      <div className="h-[114px] border-b-2 border-solid border-[#D9D9D9] w-full flex flex-1 flex-row items-center mb-[70px] bg-[#FFFFFF]">
+
+        <div>
+          <Image src={LeftTopBar} alt="Logo CITi Vet" className="ml-[50px]" /> 
+        </div>
+
+        <div className="flex flex-1 flex-col h-full justify-around items-center bg-none">
+
+            <Tabs defaultValue="account" className="flex flex-col items-center">
+              <TabsList className="bg-white text-[#242424] bg-transparent">
+                <TabsTrigger value="service" className="mr-[30px] font-normal">Atendimento</TabsTrigger>
+                <TabsTrigger value="register" className="font-normal">Cadastro</TabsTrigger>
+              </TabsList>
+
+              <div className="h-[2px] w-[80px] bg-[#50E678] rounded-full mr-[110px]">
+
+              </div>
+
+
+            </Tabs>
+
+        </div>
+
+        <div>
+          <Image src={RightTopBar} alt="CITi Slogan" className="mr-[50px]" /> 
+        </div>
+
+      </div>
+
+
       <div>
         <Image src={LogoCITi} alt="Logo citi" />
       </div>
@@ -15,6 +49,8 @@ export default function Home() {
           <strong>&hearts;</strong> by CITi
         </p>
       </div>
+      
+
     </div>
   );
 }
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,9 @@
 import LogoCITi from "./Logo.png";
+import LeftTopBar from "./LeftTopBar.svg";
+import RightTopBar from "./RightTopBar.svg";
 
-export { LogoCITi };
+export {
+    LogoCITi,
+    LeftTopBar,
+    RightTopBar
+ };
