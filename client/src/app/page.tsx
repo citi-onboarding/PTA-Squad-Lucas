3846030f61f6344bdc062dca7a66c4f9a40100db
adsonviana <adsonaviana@gmail.com>
2025-05-23 09:15:22 -0300

    feat/topBar: including page2. function and initialization adjustments

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,31 @@
 import TopBar from "@/components/topBar/";
 
-type Props = {
-  page: React.ReactNode;
-};
-
-
-export default function Home({page}: Props ) {
+export default function Home() {
   return (
 
-    <TopBar page1={page} />
-    
+    <TopBar
+    page1={
+          <div>
+            <div>
+              <div className="flex flex-col justify-center items-center">
+                <h1 className="text-black text-4xl font-bold">Page 1</h1>
+                <h1 className="text-black text-4xl font-bold">NextJS Boilerplate</h1>
+                <p className="text-black text-xl">Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}<strong>&hearts;</strong> by CITi</p>
+              </div>
+            </div>
+          </div>}
+
+    page2={
+          <div>
+            <div>
+              <div className="flex flex-col justify-center items-center">
+                <h1 className="text-red-500 text-4xl font-bold">Page 2</h1>
+                <h1 className="text-red-500 text-4xl font-bold">NextJS Boilerplate</h1>
+                <p className="text-red-500 text-xl">Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}<strong>&hearts;</strong> by CITi</p>
+              </div>
+            </div>
+          </div>}
+
+    />
   );
 }
--- client/src/components/topBar/index.tsx
+++ client/src/components/topBar/index.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
-import { LogoCITi} from "../../assets";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { LeftTopBar, RightTopBar } from "../../assets";
 
 type Props = {
   page1: React.ReactNode;
+  page2: React.ReactNode;
 };
 
-export default function topBar({page1}: Props) {
+
+export default function topBar({page1, page2}: Props) {
+
   return(
 
     <div>
-      <Tabs defaultValue="topBar" className="flex flex-col items-center bg-white">
+      
 
+      <Tabs defaultValue="service" className="flex flex-col items-center bg-white">
+        
         <TabsList className="pt-5 pb-5 bg-white text-[#242424] flex flex-1 flex-row justify-around h-[114px] border-b-2 rounded-none border-solid border-[#D9D9D9] w-full items-center mb-5">
           <Image src={LeftTopBar} alt="Logo CITi Vet"/>
 
-          <div className="pb-2 mt-6">
-            <TabsTrigger value="service" className="font-normal p-0 pb-1 mr-6  rounded-none data-[state=active]:border-b-2 data-[state=active]:border-[#50E678]">Atendimento</TabsTrigger>
+          <div className="pb-2 mt-6 ">
+            <TabsTrigger value="service" className="font-normal p-0 pb-1 mr-6 mb-0 rounded-none data-[state=active]:border-b-2 data-[state=active]:border-[#50E678]">Atendimento</TabsTrigger>
             <TabsTrigger value="register" className="font-normal p-0 pb-1 ml-6 rounded-none data-[state=active]:border-b-2 data-[state=active]:border-[#50E678]">Cadastro</TabsTrigger>
           </div>
 
@@ -26,22 +29,12 @@ export default function topBar({page1}: Props) {
         </TabsList>
 
         <TabsContent value="service">
-
-          <div>
-            <Image src={LogoCITi} alt="Logo citi" />
-            <div className="flex flex-col justify-center items-center">
-              <h1 className="text-white text-4xl font-bold">NextJS Boilerplate</h1>
-              <p className="text-white text-xl">Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}<strong>&hearts;</strong> by CITi</p>
-            </div>
-          </div>
-
-        </TabsContent>
-        
-        <TabsContent value="register" className="text-black">
-          relógio          
           {page1}
         </TabsContent>
-
+        
+        <TabsContent value="register">
+          {page2}
+        </TabsContent>
 
       </Tabs>
     </div>
