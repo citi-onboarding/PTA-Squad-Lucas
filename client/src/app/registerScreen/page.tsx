ecfda1080cd07878a3d9f43fb7dbe4ba61abcbd6
CosmoLucs <cosmolucs@gmail.com>
2025-05-24 01:51:36 -0300

    feat(registerScreen): Refactor registration form layout and improve accessibility

--- client/src/app/registerScreen/page.tsx
+++ client/src/app/registerScreen/page.tsx
@@ -1,19 +1,19 @@
 "use client"
 import React, { useState } from 'react';
 import Image from "next/image";
-import { lessThen } from '@/assets';
-import { Sheep } from '@/assets';
-import { Cat } from '@/assets';
-import { Pig } from '@/assets'
-import { Cow } from '@/assets';
-import { Horse } from '@/assets';
-import { Dog } from '@/assets';
-import { downSignal } from '@/assets';
+import { 
+  lessThen,
+  Sheep,
+  Cat,
+  Pig,
+  Cow,
+  Horse,
+  Dog,
+  downSignal
+ } from '@/assets';
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-
 
 enum PatientSpecie {
   SHEEP = "SHEEP",
@@ -28,11 +28,12 @@ type ConsultForm = {
     patientName: string;
     tutorName: string;
     species: PatientSpecie;
-    patientAge: string;
+    patientAge: string; //Tranformar para number
     consultType: string;
 };
 
-export default function Forms() {
+export default function RegisterPage() {
+
   const formSchema = z.object({
     patientName: z.string().min(1, "Nome do paciente é obrigatório"),
     tutorName: z.string().min(1, "Nome do tutor é obrigatório!"),
@@ -51,67 +52,140 @@ export default function Forms() {
   
 
   return (
-    <form onSubmit={handleSubmit(handleChange)}>~
-      <div className = "pt-40 pl-48">
+    <form onSubmit={handleSubmit(handleChange)}>
+      <div className = "pt-12 px-48">
+        
+        {/* Essa div será uma botão para retornar á página de Atendimento, crie uma função vazia por enquanto */}
         <div className = 'w-72 h-14 flex flex-row'>
           <div className = 'pt-[8px]'>
             <Image src= { lessThen } alt="Less then" className = 'w-10 h-10'/>
           </div>
-          <p className = 'text-5xl pl-4 font-bold tracking-0'>Cadastro</p>
+          <p className = 'text-5xl pl-4 font-bold'>Cadastro</p>
         </div>
 
-        <div className = 'w-[1532px] h-[644px] gap-6 flex flex-col'>
+        {/* Forms */}
+        <div className = 'w-full gap-6 flex flex-col'>
+
           <div className='flex flex-row gap-6'>
-              <div className = 'w-[754px] pt-8  h-20 flex flex-col gap-3'>
-                  <p className='font-bold text-4 tracking-0'>Nome do Paciente</p>        
-                  <input {...register("patientName")} type="text" placeholder='Digite aqui...' className = 'border border-black rounded-2xl max-w-3xl h-12 placeholder-[#D9D9D9] pt-4 pl-4 pb-4 ' />
-              </div>
-              <div className = 'w-[754px] pt-8 h-20 flex flex-col gap-3'>
-                  <p className='font-bold text-4 tracking-0'>Nome do Tutor</p>        
-                  <input {...register("tutorName")} type="text" placeholder='Digite aqui...' className = 'border border-black rounded-2xl max-w-3xl h-12 placeholder-[#D9D9D9] pt-4 pl-4 pb-4 '/>
-              </div>        
+
+            {/* Nome do Paciente */}
+            <div className = 'w-full pt-8  h-20 flex flex-col gap-3'>
+                <p className='font-bold text-4'>Nome do Paciente</p>        
+                <input {...register("patientName")} 
+                type="text" 
+                placeholder='Digite aqui...' 
+                className = 'border border-black rounded-xl h-12 placeholder-[#D9D9D9] py-4 pl-4'/>
+            </div>
+
+            {/* Nome do Tutor */}
+            <div className = 'w-full pt-8 h-20 flex flex-col gap-3'>
+                <p className='font-bold text-4'>Nome do Tutor</p>        
+                <input {...register("tutorName")} 
+                type="text" 
+                placeholder='Digite aqui...' 
+                className = 'border border-black rounded-xl h-12 placeholder-[#D9D9D9] py-4 pl-4'/>
+            </div>        
           </div>
 
-          <div className = 'w-[1042.37] h-44 pt-10 flex flex-col'>
-              <p className = 'font-bold text-4 tracking-0'>Qual é a espécie do paciente?</p>
-              <div className = 'flex flex-row pt-4 w-[1042.37] gap-[60px] h-36 px-3'>
-                <div className={`pt-2 pl-[10px] w-[120px] h-[120px] rounded-xl cursor-pointer pl-1 ${selectedSpecies === PatientSpecie.SHEEP ? 'bg-[#D9D9D9]' : ''}`} onClick={() => {setValue("species", PatientSpecie.SHEEP); setSelectedSpecies(PatientSpecie.SHEEP);}}>  
-                  <Image src= { Sheep } alt= "sheep" className = 'w-[100px] h-[100px]'/>
-                </div>
-                <div className={`pl-2 w-[120px] h-[120px] rounded-xl cursor-pointer pt-3 ${selectedSpecies === PatientSpecie.CAT ? 'bg-[#D9D9D9]' : ''}`} onClick={() => {setValue("species", PatientSpecie.CAT); setSelectedSpecies(PatientSpecie.CAT);}}>
-                  <Image src= { Cat } alt= "cat" className = 'w-[98px] h-[100px]'/>
-                </div>
-                <div className={`pl-2 w-[120px] h-[120px] rounded-xl cursor-pointer pt-3 ${selectedSpecies === PatientSpecie.PIG ? 'bg-[#D9D9D9]' : ''}`} onClick={() => {setValue("species", PatientSpecie.PIG); setSelectedSpecies(PatientSpecie.PIG);}}>
-                  <Image src= { Pig } alt= "pig" className = 'w-[100px] h-[100px]'/>
-                </div>
-                <div className={`pl-3 w-[120px] h-[120px] rounded-xl cursor-pointer pt-3 ${selectedSpecies === PatientSpecie.COW ? 'bg-[#D9D9D9]' : ''}`} onClick={() => {setValue("species", PatientSpecie.COW); setSelectedSpecies(PatientSpecie.COW);}}>
-                  <Image src= { Cow } alt= "cow" className = 'w-[100px] h-[100px]'/>
-                </div>
-                <div className={`pl-3 w-[120px] h-[120px] rounded-xl cursor-pointer pt-3 ${selectedSpecies === PatientSpecie.HORSE ? 'bg-[#D9D9D9]' : ''}`} onClick={() => {setValue("species", PatientSpecie.HORSE); setSelectedSpecies(PatientSpecie.HORSE);}}>
-                  <Image src= { Horse } alt= "horse" className = 'w-[100px] h-[100px]'/>
-                </div>
-                <div className={` w-[120px] h-[120px] rounded-xl cursor-pointer pt-3 pl-3 ${selectedSpecies === PatientSpecie.DOG ? 'bg-[#D9D9D9]' : ''}`} onClick={() => {setValue("species", PatientSpecie.DOG); setSelectedSpecies(PatientSpecie.DOG);}}>
-                  <Image src= { Dog } alt= "dog" className = 'w-[100px] h-auto pt-1'/>
-                </div>
+          {/* Espécie do Paciente */}
+          <div className = 'h-44 pt-10 flex flex-col'>
+            <p className = 'font-bold text-4'>Qual é a espécie do paciente?</p>
+
+            {/* Espécies do Paciente */}
+            <div className = 'flex flex-row pt-4 justify-start gap-14 h-36'>
+              
+              {/* Ovelha */}
+              <div 
+              className={`p-2 mt-2 w-32 h-32 rounded-xl ${selectedSpecies === PatientSpecie.SHEEP ? 'bg-[#D9D9D9]' : ''}`} 
+              onClick={() => {
+                setValue("species", PatientSpecie.SHEEP); 
+                setSelectedSpecies(PatientSpecie.SHEEP);
+              }}>  
+                <Image src={Sheep} alt="Ovelha" />
               </div>
+
+              {/* Gato */}
+              <div 
+              className={`p-2 mt-2 w-32 h-32 rounded-xl ${selectedSpecies === PatientSpecie.CAT ? 'bg-[#D9D9D9]' : ''}`} 
+              onClick={() => {
+                setValue("species", PatientSpecie.CAT); 
+                setSelectedSpecies(PatientSpecie.CAT);
+              }}>
+                <Image src={Cat} alt="Gato" />
+              </div>
+              
+              {/* Porco */}
+              <div 
+              className={`p-2 mt-2 w-32 h-32 rounded-xl ${selectedSpecies === PatientSpecie.PIG ? 'bg-[#D9D9D9]' : ''}`} 
+              onClick={() => {
+                setValue("species", PatientSpecie.PIG); 
+                setSelectedSpecies(PatientSpecie.PIG);
+              }}>
+                <Image src={Pig} alt="Porco" />
+              </div>
+
+              {/* Vaca */}
+              <div 
+              className={`p-2 mt-2 w-32 h-32 rounded-xl ${selectedSpecies === PatientSpecie.COW ? 'bg-[#D9D9D9]' : ''}`} 
+              onClick={() => {
+                setValue("species", PatientSpecie.COW); 
+                setSelectedSpecies(PatientSpecie.COW);
+              }}>
+                <Image src={Cow} alt="Vaca" />
+              </div>
+
+              {/* Cavalo */}
+              <div 
+              className={`p-2 mt-2 w-32 h-32 rounded-xl ${selectedSpecies === PatientSpecie.HORSE ? 'bg-[#D9D9D9]' : ''}`} 
+              onClick={() => {
+                setValue("species", PatientSpecie.HORSE); 
+                setSelectedSpecies(PatientSpecie.HORSE);
+              }}>
+                <Image src={Horse} alt="Cavalo" />
+              </div>
+
+              <div className={`p-2 mt-2 w-32 h-32 rounded-xl ${selectedSpecies === PatientSpecie.DOG ? 'bg-[#D9D9D9]' : ''}`} 
+              onClick={() => {
+                setValue("species", PatientSpecie.DOG); 
+                setSelectedSpecies(PatientSpecie.DOG);
+              }}>
+                <Image src= { Dog } alt= "Cachorro"/>
+              </div>
+            </div>
           </div>
 
-          <div className='flex flex-row gap-6 '>
-              <div className = 'w-[754px] pt-8  h-20 flex flex-col gap-3'>
-                  <p className='font-bold text-4 tracking-0'>Idade do Paciente</p>        
-                  <input {...register("patientAge")} type="text" placeholder='Digite aqui...' className = 'border border-black rounded-2xl max-w-3xl h-12 placeholder-[#D9D9D9] pt-4 pl-4 pb-4 ' />
-              </div>
-              <div className='w-[754px] pt-8 h-20 flex flex-col gap-3'>
-                <p className='font-bold text-4 tracking-0'>Tipo de consulta</p>
-                <div className='flex items-center border border-black rounded-2xl w-full h-12 pl-4 pr-4 bg-white'>
-                  <input {...register("consultType")} type="text" placeholder="Selecione aqui" className="w-full h-full outline-none placeholder-[#D9D9D9] pt-4  pb-4"/>
-                  <Image src={downSignal} alt="Down Signal" className="ml-2"/>
-                </div>
-              </div>       
+          <div className='flex flex-row gap-6'>
+
+            {/* Idade do Paciente */}
+            <div className = 'w-[754px] pt-8  h-20 flex flex-col gap-3'>
+                <p className='font-bold text-4'>Idade do Paciente</p>        
+                <input {...register("patientAge")} 
+                type="number" 
+                placeholder='Digite aqui...' 
+                className = 'border border-black rounded-xl h-12 placeholder-[#D9D9D9] py-4 pl-4'/>
+            </div>
+
+            {/* Tipo de Consulta */}
+            <div className='w-[754px] pt-8 h-20 flex flex-col gap-3'>
+              <p className='font-bold text-4'>Tipo de consulta</p>
+              <select
+                {...register("consultType")}
+                className="border border-black rounded-xl py-3 pl-4 text-base box-border"
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Selecione o tipo de consulta
+                </option>
+                <option value="FIRST">Primeira Consulta</option>
+                <option value="VACINATION">Vacinação</option>
+                <option value="RETURN">Retorno</option>
+                <option value="CHECKUP">Check-up</option>
+              </select>
+            </div>
+
           </div>
         </div>
       </div>
     </form>
   );
 }
-
