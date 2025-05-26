51e2eff111366fd0e552d3fb2d23084e1d219894
adsonviana <adsonaviana@gmail.com>
2025-05-26 18:57:15 -0300

    feat/buttons: variables and function adjustments

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,8 @@
 'use client';
 import { TopBar } from "@/components";
 import { Buttons } from "@/components";
-import { Bike } from 'lucide-react';
 
 export default function Home() {
-
-  const add = () => {
-    console.log("foi");
-    return
-  }
-
   return (
 
     <TopBar
