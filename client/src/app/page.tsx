a248c16f537020b1637f8cec9c995de6c2230a46
CosmoLucs <cosmolucs@gmail.com>
2025-05-28 00:35:47 -0300

    fix(Deploy#1):Update Home component to render RegisterPage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 import { TopBar } from "@/components";
+import RegisterPage from "./registerScreen/page";
 
 export default function Home() {
   return (
     <TopBar
       page1={<></>}
-      page2={<></>}
+      page2={<RegisterPage />}
     />
   );
 }
