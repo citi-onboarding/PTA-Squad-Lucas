a0ab8d6517446df9d955d15725f40b788a321b0d
Felipe Alves <felipemaranalvees40@gmail.com>
2025-06-01 23:46:48 -0300

    fix(feat/consultation_card): forgot to remove mock from the page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,60 +5,7 @@ import ConsulCard from "@/components/consulCard"
 export default function Home() {
   return (
     <TopBar
-      page1={<>
-      <div className="flex flex-wrap gap-4">
-        <ConsulCard
-        id={1}
-        DateTime="2025-05-23T14:30:00.000Z"
-        typeConsul="FIRST"
-        doctorName="Dr. Smith"
-        pacientName="Luna"
-        pacientTutorName="João"
-        pacientAge={5}
-        pacientSpecie={"CAT"}
-        onClick={() => console.log("clicado")}></ConsulCard>
-        <ConsulCard
-        id={1}
-        DateTime="2025-05-23T14:30:00.000Z"
-        typeConsul="RETURN"
-        doctorName="Dr. Smith"
-        pacientName="Luna"
-        pacientTutorName="João"
-        pacientAge={5}
-        pacientSpecie={"SHEEP"}
-        onClick={() => console.log("clicado")}></ConsulCard>
-        <ConsulCard
-        id={1}
-        DateTime="2025-05-23T14:30:00.000Z"
-        typeConsul="CHECKUP"
-        doctorName="Dr. Smith"
-        pacientName="Luna"
-        pacientTutorName="João"
-        pacientAge={5}
-        pacientSpecie={"COW"}
-        onClick={() => console.log("clicado")}></ConsulCard>
-        <ConsulCard
-        id={1}
-        DateTime="2025-05-23T14:30:00.000Z"
-        typeConsul="VACINATION"
-        doctorName="Dr. Smith"
-        pacientName="Luna"
-        pacientTutorName="João"
-        pacientAge={5}
-        pacientSpecie={"HORSE"}
-        onClick={() => console.log("clicado")}></ConsulCard>
-        <ConsulCard
-        id={1}
-        DateTime="2025-06-27T14:30:00.000Z"
-        typeConsul="CHECKUPdafsghk"
-        doctorName="Dr. Smith"
-        pacientName="Luna"
-        pacientTutorName="João"
-        pacientAge={5}
-        pacientSpecie={"asfdgfgfhg"}
-        onClick={() => console.log("clicado")}></ConsulCard>
-      </div>
-      </>}
+      page1={<></>}
       page2={<></>}
     />
   );
