62729d65581684b40c26ca16e44190e590c1a81c
Felipe Alves <felipemaranalvees40@gmail.com>
2025-06-06 18:24:56 -0300

    feat(integration): initial com

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,13 +1,13 @@
 import topBar from "./topBar";
 import button from "./Button"
-import consultCard from "./consultCard"
+import consulCard from "./consulCard"
 import History from "./History";
 import ConsultCardHistory from "./ConsultCardHistory";
 
 export {
      topBar as TopBar,
      button as Button,
-     consultCard as ConsultCard,
+     consulCard as ConsulCard,
      History as History,
      ConsultCardHistory as ConsultCardHistory,
 };
